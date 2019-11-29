b002690db01111c42aacc06f81ff9ec09643a32b
ksodhi2 <ksodhi2@illinois.edu>
2019-11-28 19:51:15 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cs125finalproject/ui/gened/GenedFragment.java
@@ -0,0 +1,20 @@
+package com.example.cs125finalproject.ui.gened;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.cs125finalproject.R;
+
+public class GenedFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_gened, container, false);
+        return root;
+    }
+}
